0455dfab101ae12ecb789aac119fa08809187466
Szymon Wnuk <szymon.wnuk@icloud.com>
2022-10-11 14:54:03 +0200

    Initial Commit

---
+++ PizzaAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PizzaAnimation
+//
+//  Created by Szymon Wnuk on 11/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaAnimation/PizzaAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAnimationApp.swift
+//  PizzaAnimation
+//
+//  Created by Szymon Wnuk on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
